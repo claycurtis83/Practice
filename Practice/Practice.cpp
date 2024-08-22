271a987332e724a6e226d93a53da320ed2f48165
Clay Curtis <Clay Curtis@DESKTOP-RNSSGDN>
2024-08-22 15:33:46 -0500

    Add project files.

---
+++ Practice/Practice.cpp
@@ -0,0 +1,13 @@
+// Name: Clay Curtis
+// Date: 8/22/2024
+// Program: Practice.cpp 
+// Description: This is to practice using github
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+}
+
